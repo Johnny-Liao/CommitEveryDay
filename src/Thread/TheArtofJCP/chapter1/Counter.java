508b18cf0e05bff2850920cecd36cb53b84c6cd9
Johnny-Liao <1020157553@qq.com>
2015-10-11 14:51:28 +0800

    Demo self rotate CAS counter.

--- src/Thread/TheArtofJCP/chapter1/Counter.java
+++ src/Thread/TheArtofJCP/chapter1/Counter.java
@@ -1,7 +1,69 @@
 package Thread.TheArtofJCP.chapter1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
+ * 演示自旋CAS操作计数器方法（safeConut）和非线程安全计数器（count）
  * Created by johnny on 15-10-11.
  */
 public class Counter {
+    private AtomicInteger atomicI = new AtomicInteger(0);
+    private int i = 0;
+
+    /**
+     * 使用CAS实现线程安全计数器
+     */
+    private void safeCount() {
+        for (;;) {
+            int i = atomicI.get();
+            boolean suc = atomicI.compareAndSet(i, ++i);        // 循环CAS
+            if(suc) {
+                break;
+            }
+        }
+    }
+
+    /**
+     * 非线程安全计数器
+     */
+    private void count() {
+        i++;
+    }
+
+    public static void main(String[] args) {
+        final Counter counter = new Counter();
+        List<Thread> ts = new ArrayList<Thread>(600);
+        long start = System.currentTimeMillis();
+        for (int j = 0; j < 100; j++) {
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 10000; i++) {
+                        counter.count();
+                        counter.safeCount();
+                    }
+                }
+            });
+            ts.add(t);
+        }
+
+        for (Thread t : ts) {
+            t.start();
+        }
+
+        // Waiting all thread finish.
+        for (Thread t : ts) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(counter.i);
+        System.out.println(counter.atomicI.get());
+        System.out.println(System.currentTimeMillis() - start);
+    }
 }
