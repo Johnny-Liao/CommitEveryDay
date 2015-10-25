95fa1e4f674299a76f9c3bd71962d90504f4cd7e
Johnny-Liao <1020157553@qq.com>
2015-10-25 16:33:55 +0800

    Implements JoinCountDownLatchTest.

---
+++ src/Thread/TheArtofJCP/chapter4/ConnectionDriver.java
@@ -0,0 +1,31 @@
+package Thread.TheArtofJCP.chapter4;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.sql.Connection;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 示例：仅仅在commit方法被调用时休眠100毫秒
+ * Created by johnny on 15-10-14.
+ */
+public class ConnectionDriver {
+
+    static class ConnectionHandler implements InvocationHandler {
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            if (method.getName().equals("commit")) {
+                TimeUnit.MILLISECONDS.sleep(100);
+            }
+            return null;
+        }
+
+    }
+
+    public static Connection createConnection() {
+        return (Connection) Proxy.newProxyInstance(ConnectionDriver.class.getClassLoader(),
+                new Class<?>[]{Connection.class}, new ConnectionHandler());
+    }
+}
