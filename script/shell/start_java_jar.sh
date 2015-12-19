feb7c64549dae3d7fbb11126aaf2764ad9ffdbaa
Johnny-Liao <1020157553@qq.com>
2015-12-19 22:42:20 +0800

    script to start java program.

--- script/shell/start_java_jar.sh
+++ script/shell/start_java_jar.sh
@@ -1,26 +1,26 @@
 #! /bin/bash
 
 # program about
-jarFileName=com.bj58.search.tools.jar    # your program name
-jarCommands=offline.conf                 # runtime commands
+jarFileName=***.jar    # your program name
+jarCommands=***                 # runtime commands
 
 # path
-programPath=/home/work/liaoshihao		 # program path
-logdir=/home/work/liaoshihao/log         # log path
+programPath=/**/**/johnny		 # program path
+logdir=/**/**/johnny/log         # log path
 
 # check program process
 runcount=`ps -ef | grep "java -jar ${jarFileName} ${jarCommands}" | grep -v grep | wc -l`
 
 if [ $runcount -lt 1 ] ; then
-        echo `date` +":Warning ${jarFileName} is not runnning , start..........." >> ${logdir}/search_tools.log
+        echo `date` + " : Warning ${jarFileName} is not runnning , start..........." >> ${logdir}/start_pro.log
         source /etc/profile
         cd $programPath
         nohup java -jar ${jarFileName} ${jarCommands} >> ${logdir}/pragram_runtime.log 2>&1 &
 else
-        echo `date` +"serch.tools is running : ok" >> ${logdir}/search_tools.log
+        echo `date` +"serch.tools is running : ok" >> ${logdir}/start_pro.log
 fi
 
 exit 1
 
-# Timing start the script
-# 00 4 * * * /bin/sh /home/work/liaoshihao/tools_start.sh > /home/work/liaoshihao/log/crontab.log 2>&1
+# Timing start the script at 4:00 am every day.
+# 00 4 * * * /bin/sh /**/**/johnny/start_java_jar.sh > /**/**/johnny/log/crontab.log 2>&1
