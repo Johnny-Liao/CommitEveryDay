692d6dd47b3f87d8958ac1b1e6856fd62272e7c8
Johnny-Liao <1020157553@qq.com>
2015-12-28 19:51:22 +0800

    Format for read.

--- src/main/java/org/lsh/net/UDPServer.java
+++ src/main/java/org/lsh/net/UDPServer.java
@@ -11,26 +11,26 @@ import java.net.DatagramSocket;
 
 public class UDPServer {
     public static void main(String[] args) throws IOException {
-        String str_send = "Hello UDP Client";
+        String str_send = "Hello UDP Client ! ";
         byte[] buf = new byte[1024];
         // 服务端在3000端口监听接收到的数据
         DatagramSocket ds = new DatagramSocket(3000);
         // 接收从客户端发送过来的数据
         DatagramPacket dp_receive = new DatagramPacket(buf, 1024);
-        System.out.println("server is on，waiting for client to send data......");
+        System.out.println("Server is on , waiting for client to send data ......");
         boolean flag = true;
         while (flag) {
             // 服务器端接收来自客户端的数据
             ds.receive(dp_receive);
-            System.out.println("server received data from client：");
-            String str_receive = new String(dp_receive.getData(), 0, dp_receive.getLength()) +
-                    " from " + dp_receive.getAddress().getHostAddress() + ":" + dp_receive.getPort();
+
+            System.out.println("Server received data from client : ");
+            String str_receive = new String(dp_receive.getData(), 0, dp_receive.getLength()) + " from " + dp_receive.getAddress().getHostAddress() + ":" + dp_receive.getPort();
             System.out.println(str_receive);
+
+
             // 数据发动到客户端的9000端口
             DatagramPacket dp_send = new DatagramPacket(str_send.getBytes(), str_send.length(), dp_receive.getAddress(), 9000);
             ds.send(dp_send);
-            // 由于dp_receive在接收了数据之后，其内部消息长度值会变为实际接收的消息的字节数，
-            // 所以这里要将dp_receive的内部消息长度重新置为1024
             dp_receive.setLength(1024);
         }
         ds.close();
